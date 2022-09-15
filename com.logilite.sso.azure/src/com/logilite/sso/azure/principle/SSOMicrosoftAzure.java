333bdf7c02d5f2cbd70f7723c8cc3b3fcf8816ca
logilitedev <deepak@logilite.com>
2022-09-15 18:48:16 +0530

    Microsoft Azure AD SSO custom plugin.

---
+++ com.logilite.sso.azure/src/com/logilite/sso/azure/principle/SSOMicrosoftAzure.java
@@ -0,0 +1,121 @@
+/******************************************************************************
+ * Copyright (C) 2016 Logilite Technologies LLP								  *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *****************************************************************************/
+package com.logilite.sso.azure.principle;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.adempiere.webui.sso.ISSOPrinciple;
+import org.compiere.model.I_SSO_PrincipleConfig;
+import org.compiere.model.MSysConfig;
+import org.compiere.util.Language;
+
+import com.microsoft.aad.msal4j.IAuthenticationResult;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.JWTParser;
+
+/**
+ * Microsoft Azure AD SSO login and authentication
+ * 
+ * @author Logilite Technologies
+ */
+public class SSOMicrosoftAzure implements ISSOPrinciple
+{
+
+	AuthHelper authHelper = null;
+
+	public SSOMicrosoftAzure(I_SSO_PrincipleConfig config)
+	{
+		authHelper = new AuthHelper(config);
+	}
+
+	@Override
+	public boolean hasAuthenticationCode(HttpServletRequest request, HttpServletResponse response)
+	{
+		Map<String, String[]> httpParameters = request.getParameterMap();
+		boolean containIdToken = httpParameters.containsKey("id_token");
+		boolean containsCode = httpParameters.containsKey("code");
+
+		return containIdToken || containsCode;
+	}
+
+	@Override
+	public void getAuthenticationToken(HttpServletRequest request, HttpServletResponse response) throws Throwable
+	{
+		if (SessionManagementHelper.getAuthSessionObject(request) != null)
+			return;
+
+		String currentUri = request.getRequestURL().toString();
+		String queryStr = request.getQueryString();
+		String fullUrl = currentUri + (queryStr != null ? "?" + queryStr : "");
+		authHelper.processAuthenticationCodeRedirect(request, currentUri, fullUrl);
+		((HttpServletResponse) response).sendRedirect(currentUri);
+	}
+
+	@Override
+	public boolean isAuthenticated(HttpServletRequest request, HttpServletResponse response)
+	{
+		if (request.getSession() == null)
+			return false;
+		return request.getSession().getAttribute(ISSOPrinciple.SSO_PRINCIPLE_SESSION_NAME) != null;
+	}
+
+	@Override
+	public void redirectForAuthentication(HttpServletRequest request, HttpServletResponse response) throws IOException
+	{
+		authHelper.sendAuthRedirect(request, response, null, authHelper.getRedirectURIs());
+	}
+
+	@Override
+	public boolean isAccessTokenExpired(HttpServletRequest request, HttpServletResponse response)
+	{
+		IAuthenticationResult result = SessionManagementHelper.getAuthSessionObject(request);
+		return result.expiresOnDate().before(new Date());
+	}
+
+	@Override
+	public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws Throwable
+	{
+		IAuthenticationResult authResult = authHelper.getAuthResultBySilentFlow(request, response);
+		SessionManagementHelper.setSessionPrincipal(request, authResult);
+	}
+
+	@Override
+	public void removePrincipleFromSession(HttpServletRequest httpRequest)
+	{
+		SessionManagementHelper.removePrincipalFromSession(httpRequest);
+	}
+
+	@Override
+	public String getUserName(Object result) throws ParseException
+	{
+		boolean email_login = MSysConfig.getBooleanValue(MSysConfig.USE_EMAIL_FOR_LOGIN, false);
+		JWTClaimsSet jwtClaimsSet = JWTParser.parse(((IAuthenticationResult) result).idToken()).getJWTClaimsSet();
+		if (email_login)
+			return (String) jwtClaimsSet.getClaim("email");
+		else
+			return (String) jwtClaimsSet.getClaim("name");
+
+	}
+
+	@Override
+	public Language getLanguage(Object result) throws ParseException
+	{
+		return Language.getBaseLanguage();
+	}
+}
---
+++ com.logilite.sso.azure/src/com/logilite/sso/azure/principle/SessionManagementHelper.java
@@ -0,0 +1,120 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.logilite.sso.azure.principle;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.adempiere.webui.sso.ISSOPrinciple;
+import org.compiere.util.Util;
+
+import com.microsoft.aad.msal4j.IAuthenticationResult;
+
+/**
+ * Helpers for managing session
+ */
+public class SessionManagementHelper
+{
+
+	public static final String	STATE							= "state";
+	public static final String	STATES							= "states";
+	public static final Integer	STATE_TTL						= 3600;
+
+	public static final String	FAILED_TO_VALIDATE_MESSAGE		= "Failed to validate data received from Authorization service - ";
+	public static final String	TOKEN_CACHE_SESSION_ATTRIBUTE	= "token_cache";
+
+	static StateData validateState(HttpSession session, String state) throws Exception
+	{
+		if (!Util.isEmpty(state))
+		{
+			StateData stateDataInSession = removeStateFromSession(session, state);
+			if (stateDataInSession != null)
+			{
+				return stateDataInSession;
+			}
+		}
+		throw new Exception(FAILED_TO_VALIDATE_MESSAGE + "could not validate state");
+	}
+
+	private static StateData removeStateFromSession(HttpSession session, String state)
+	{
+		@SuppressWarnings("unchecked")
+		Map<String, StateData> states = (Map<String, StateData>) session.getAttribute(STATES);
+		if (states != null)
+		{
+			eliminateExpiredStates(states);
+			StateData stateData = states.get(state);
+			if (stateData != null)
+			{
+				states.remove(state);
+				return stateData;
+			}
+		}
+		return null;
+	}
+
+	private static void eliminateExpiredStates(Map<String, StateData> map)
+	{
+		Iterator<Map.Entry<String, StateData>> it = map.entrySet().iterator();
+
+		Date currTime = new Date();
+		while (it.hasNext())
+		{
+			Map.Entry<String, StateData> entry = it.next();
+			long diffInSeconds = TimeUnit.MILLISECONDS.toSeconds(currTime.getTime() - entry.getValue().getExpirationDate().getTime());
+
+			if (diffInSeconds > STATE_TTL)
+			{
+				it.remove();
+			}
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	static void storeStateAndNonceInSession(HttpSession session, String state, String nonce)
+	{
+
+		// state parameter to validate response from Authorization server and nonce parameter to
+		// validate idToken
+		if (session.getAttribute(STATES) == null)
+		{
+			session.setAttribute(STATES, new HashMap<String, StateData>());
+		}
+		((Map<String, StateData>) session.getAttribute(STATES)).put(state, new StateData(nonce, new Date()));
+	}
+
+	static void storeTokenCacheInSession(HttpServletRequest httpServletRequest, String tokenCache)
+	{
+		httpServletRequest.getSession().setAttribute(TOKEN_CACHE_SESSION_ATTRIBUTE, tokenCache);
+	}
+
+	public static void setSessionPrincipal(HttpServletRequest httpRequest, IAuthenticationResult result)
+	{
+		httpRequest.getSession().setAttribute(ISSOPrinciple.SSO_PRINCIPLE_SESSION_NAME, result);
+	}
+
+	public static void removePrincipalFromSession(HttpServletRequest httpRequest)
+	{
+		httpRequest.getSession().removeAttribute(ISSOPrinciple.SSO_PRINCIPLE_SESSION_NAME);
+	}
+
+	public static IAuthenticationResult getAuthSessionObject(HttpServletRequest request)
+	{
+		Object principalSession = request.getSession().getAttribute(ISSOPrinciple.SSO_PRINCIPLE_SESSION_NAME);
+		if (principalSession instanceof IAuthenticationResult)
+		{
+			return (IAuthenticationResult) principalSession;
+		}
+		else
+		{
+			return null;
+		}
+	}
+}
